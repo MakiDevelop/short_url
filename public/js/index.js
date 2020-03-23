036f7714bc2c0345a081d6f2bce1266ebd11b976
Onion <wnrsfnw@gmail.com>
2020-03-23 23:59:59 +0800

    js remove url param for fbclid

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,23 @@ $(function () {
         console.log(isUrl($(this).val()));
         let url = new URL($(this).val());
         console.log(url.hostname);
+        console.log(url.search);
+        console.log(url.pathname);
+        console.log(url.searchParams);
+
+        let params = url.searchParams;
+        // var new_params = {};
+        // for (let pair of params.entries()) {
+        //     console.log(`key: ${pair[0]}, value: ${pair[1]}`)
+        //     if (pair[0] != 'fbclid') {
+
+        //     }
+        // }
+        params.delete('fbclid');
+        console.log(params.toString());
+        url.search = params;
+        console.log(url.href);
+        $(this).val(url.href);
     });
     $('#send').click(function(){
         
