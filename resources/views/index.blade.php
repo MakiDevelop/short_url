b6cc872e29f22c330c3d50aa43d523abdc725abe
Onion <onion@babyhome.com.tw>
2020-03-27 14:14:22 +0800

    沒登入簡易版短網址

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,16 +17,25 @@
 
         <div class="form-group row">
             <div class="col-sm-10">
-                <input type="url" class="form-control form-control-lg" id="url" name="url">
+                <input type="url" class="form-control form-control-lg" id="url" name="url" placeholder="網址">
             </div>
             <div class="col-sm-2">
                 <button type="button" id="send" class="btn btn-primary mb-2 form-control-lg">提交</button>
             </div>
         </div>
     </form>
-    <div class="card" id="short_url">
+    <div class="card d-none" id="short_url">
         <div class="card-body">
-          This is some text within a card body.
+            <div class="col-sm-12 text-right">
+                <div id="url_text" class="d-inline p-8"></div>
+                <div class="d-inline p-2">
+                    <button type="button" id="copy" class="btn btn-primary">複製</button>
+                    <button type="button" id="qrcode" class="btn btn-primary" data-toggle="collapse" data-target="#collapseQRCode" aria-expanded="false" aria-controls="collapseQRCode">QRCode</button>
+                </div>
+            </div>
+            <div class="col-sm-12 collapse" id="collapseQRCode">
+
+            </div>
         </div>
     </div>
 </div>
