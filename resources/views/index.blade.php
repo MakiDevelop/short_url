1a8cab71fee46035020b91e3e0d492a9a255a4e7
Onion <onion@babyhome.com.tw>
2020-03-27 17:14:34 +0800

    移不需的html

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,17 +4,6 @@
 <div class="jumbotron"> 
     <form action="" method="post" class="">
         @csrf
-        
-        {{-- <div class="form-group mb-2">
-            <label for="staticEmail2" class="sr-only">Email</label>
-            <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="email@example.com">
-        </div> --}}
-        {{-- <div class="form-group mx-sm-3 mb-2">
-            <label for="inputPassword2" class="sr-only">網址</label>
-            <input type="url" class="form-control" id="url" name="url" placeholder="網址">
-        </div>
-        <button type="button" id="send" class="btn btn-primary mb-2">Confirm identity</button> --}}
-
         <div class="form-group row">
             <div class="col-sm-10">
                 <input type="url" class="form-control form-control-lg" id="url" name="url" placeholder="網址">
