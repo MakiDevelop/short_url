cceb91c87e08b34a11490a0622e3ba3f0d0be442
Onion <onion@babyhome.com.tw>
2020-03-31 17:11:21 +0800

    login view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="jumbotron"> 
+<div class="card-body"> 
     
+
+    <button class="btn btn-block btn-google">
+        <img src="https://developers.google.com/identity/images/g-logo.png">
+        <span class="ml-2">Sign in with Google</span>
+    </button>
+    <button class="btn btn-primary btn-block btn-facebook">
+        <i class="fab fa-facebook-square"></i>
+        <span class="ml-2">Sign in with Facebook</span>
+    </button>
+
+
 </div>
 @endsection
