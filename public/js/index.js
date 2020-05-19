acd13b3ac6192fefa6b7ae7d610976635d153ffb
Onion <wnrsfnw@gmail.com>
2020-05-19 23:32:10 +0800

    修正重複縮址會一直出現qrcode問題

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
-$(function () {
-    $('#url').change(function(){
+$(function() {
+    $('#url').change(function() {
         if (App.isUrl($(this).val())) {
             let url = new URL($(this).val());
             let params = url.searchParams;
@@ -10,31 +10,37 @@ $(function () {
             }
         }
     });
-    $('#send').click(function(){
+    $('#send').click(function() {
         var url = '/index/short_url',
             method = 'POST',
             data = $('form').serialize(),
-            callbackSuccess = function (response) {
-                console.log(response);
+            callbackSuccess = function(response) {
+                // console.log(response);
                 if ($('#error_alert').is(':visible')) {
                     $('#error_alert').attr('hidden', 'hidden');
                 }
-                
+
                 if ($('#send').length > 0) {
-					$('#send').prop('disabled', false);
+                    $('#send').prop('disabled', false);
                 }
                 if (response.success) {
+                    if ($('#collapseQRCode').find('img').length > 0) {
+                        $('#collapseQRCode').find('img').remove();
+                    }
                     $('#short_url').removeClass('d-none');
                     $('#url_text').text(response.short_url);
 
                     var img_url = 'https://chart.googleapis.com/chart?cht=qr&chs=200x200&choe=UTF-8&chl=' + encodeURIComponent(response.short_url);
-                    $('#collapseQRCode').prepend('<img src="'+ img_url +'" />');
+                    $('#collapseQRCode').prepend('<img src="' + img_url + '" />');
                 }
             };
+        if ($('#short_url').is(':visible')) {
+            $('#short_url').addClass('d-none');
+        }
         App.ajax(url, method, data, callbackSuccess);
     });
 
-    $('#copy').click(function(){
+    $('#copy').click(function() {
         App.copyToClipboard('url_text')
     });
 });
